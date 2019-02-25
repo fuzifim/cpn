f0a179d7e3cdb5d96cf842fcbfceea5db7187c80
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-02-25 16:40:21 +0700

    fix ads

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -16,7 +16,13 @@
     @yield('css')
 </head>
 <body>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><script> (adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-6739685874678212", enable_page_level_ads: true });</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-6739685874678212",
+        enable_page_level_ads: true
+    });
+</script>
 @yield('header')
 @yield('content')
 <!-- Optional JavaScript -->
